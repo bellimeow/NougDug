d511c566da54221f628cb80ecaf052d2d378e49f
Sebastian Lundqvist <seblu114@student.liu.se>
2017-12-05 11:19:09 +0100

    Empty Classes and CLion

---
+++ cmake-build-debug/CMakeFiles/NougDug.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NougDug.dir/main.cc.o"
+  "NougDug.pdb"
+  "NougDug"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NougDug.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/NougDug.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for NougDug.
+# This may be replaced when dependencies are built.
