f4434ca7234507ae79254f5afe9ef97f61142882
Isabella Delgado <isade842@student.liu.se>
2017-12-05 12:21:09 +0100

    Lade till alla dokument som vi skrivit i ShareLatex, designspec, kravspec, statusrapport och backlog.

---
+++ Character.h
@@ -0,0 +1,9 @@
+
+
+class Character
+{
+private:
+    
+
+public:
+};
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/seblu114/TDP005/NougDug
+# Install script for directory: /home/isade842/Documents/TDP005/NougDug
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/seblu114/TDP005/NougDug/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/isade842/Documents/TDP005/NougDug/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
