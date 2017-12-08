523cc5bf377103fbd328ecf35b3ee08cc803682f
Isabella Delgado <isade842@student.liu.se>
2017-12-08 16:09:08 +0100

    Merge branch 'dev' of gitlab.ida.liu.se:seblu114/NougDug into dev
    # Conflicts:
    #	.idea/workspace.xml

--- Sprite.cpp
+++ Sprite.cpp
@@ -5,16 +5,17 @@
 
 #include <string>
 #include "Sprite.h"
+#include <SFML/Graphics.hpp>
 
-Sprite::Sprite(const std::string &sprite_sheet_path)
-        : sprite_sheet_path(sprite_sheet_path)
+Sprite::Sprite()
 {
 
 }
 
 void Sprite::draw()
 {
-
+    sf::Sprite sprite;
+    sprite.setTexture(*sprite_sheets["player"]);
 }
 
 sf::Texture Sprite::get_sprite_sheet(std::string path)
@@ -24,5 +25,17 @@ sf::Texture Sprite::get_sprite_sheet(std::string path)
 
 void Sprite::animate()
 {
+
+}
+
+
+void Sprite::load_sprite_sheets()
+{
+    sf::Texture player_sprite_sheet;
+    if (!player_sprite_sheet.loadFromFile(sprite_sheet_path + "deathp.png" ))
+    {
+        sprite_sheets.insert(std::pair<std::string, sf::Texture*>("player", &player_sprite_sheet));
+    }
+
 
 }
--- Sprite.h
+++ Sprite.h
@@ -5,15 +5,16 @@
 #include <vector>
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/System/Clock.hpp>
+#include <map>
 
 class Sprite
 {
     protected:
         struct coordinate;
         sf::Clock animation_clock;
-        std::string sprite_sheet_path;
         sf::Texture texture;
         std::vector<coordinate> sprite_position;
+        std::map< std::string, sf::Texture*> sprite_sheets;
 
         struct coordinate
         {
@@ -22,10 +23,13 @@ class Sprite
         };
 
     public:
-        Sprite(const std::string &sprite_sheet_path); //implementera default konstruktor
+        Sprite(); //implementera default konstruktor
+
+        static std::string sprite_sheet_path{"/home/isade842/Documents/TDP005/NougDug/documents/Nougdug"};
 
         void draw();
         sf::Texture get_sprite_sheet( std::string path);
+        void Sprite::load_sprite_sheets();
 
         virtual void animate();
 };
